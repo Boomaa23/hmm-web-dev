def758ae8f34179bc9eb412c2f0db432338f7fd6
Boomaa23 <cmo93003@yahoo.com>
2019-02-08 01:56:07 -0800

    Landing page, menubar, and tiling setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="index-style.css">
+</head>
+<body>
+<div class="body-container">
+	<div class="menubar-container">
+		<?php include('menubar.html'); ?>
+	</div>
+	<div class="main-image">
+		<img src="web_images/gallery/">
+	</div>
+	<div class="body-panel rowA col1">
+		<h1>Hello there</h1>
+		<p>testing testing</p>
+	</div>
+	<div class="body-panel rowA col2">
+		<h1>Hello there</h1>
+		<p>testing testing</p>
+	</div>
+</div>
+</body>
+</html>
